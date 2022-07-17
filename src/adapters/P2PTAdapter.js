09ddf7562cb3012d0ba9c6f6897bae6e5e7da875
Greg Fodor <gfodor@gmail.com>
2022-07-17 06:38:28 -0700

    Create a fork of webtorrent tracker, fetch and store tracker list

--- src/adapters/P2PTAdapter.js
+++ src/adapters/P2PTAdapter.js
@@ -1,6 +1,51 @@
 const P2PT = require('./p2pt');
 const { hexToBytes } = require('../utils');
 
+const TRACKER_LIST_FETCH_INTERVAL_MS = 7 * 24 * 60 * 60 * 1000;
+
+const TRACKER_LISTS = [
+  "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ws.txt",
+  "https://ngosang.github.io/trackerslist/trackers_all_ws.txt",
+  "https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_all_ws.txt"
+]
+
+const FALLTHROUGH_TRACKERS = [
+  "wss://tracker.files.fm:7073/announce",
+  "wss://tracker.btorrent.xyz",
+  "wss://spacetradersapi-chatbox.herokuapp.com:443/announce"
+];
+
+const trackerList = new Promise(res => {
+  const lastTrackerList = localStorage.getItem("_trackerList");
+
+  if (lastTrackerList) {
+    const { trackers, lastFetchedAt } = JSON.parse(lastTrackerList);
+
+    if (performance.now() - lastFetchedAt < TRACKER_LIST_FETCH_INTERVAL_MS) {
+      return res(trackers);
+    }
+  }
+
+  const finish = (response) => {
+    response.text().then(text => {
+      const trackers = text.split("\n").filter(x => x.trim().length > 0);
+      localStorage.setItem("_trackerList", JSON.stringify({ lastFetchedAt: performance.now(), trackers }));
+
+      res(trackers);
+    });
+  };
+
+  // ugly w/e
+  fetch(TRACKER_LISTS[0]).then(finish)
+  .catch(() => {
+    fetch(TRACKER_LISTS[1]).then(finish)
+    .catch(() => {
+      fetch(TRACKER_LISTS[2]).then(finish)
+        .catch(() => res(FALLTHROUGH_TRACKERS))
+    });
+  })
+});
+
 class P2PTAdapter {
   constructor() {
     this.room = null;
@@ -39,44 +84,38 @@ class P2PTAdapter {
     this.room = room;
 
     return new Promise(res => {
-      this.leaveRoom();
+      trackerList.then(trackers => {
+        this.leaveRoom();
+      
+        this.p2pt = new P2PT(trackers);
 
-      this.p2pt = new P2PT(
-        [
-        "wss://tracker.files.fm:7073/announce",
-        "wss://tracker.btorrent.xyz",
-        "wss://spacetradersapi-chatbox.herokuapp.com:443/announce",
-        "wss://qot.abiir.top:443/announce",
-        "ws://tracker.files.fm:7072/announce"
-        ]
-      )
+        this.p2pt.setPeerIdBytes(new Uint8Array(hexToBytes(this.clientId)));
 
-      this.p2pt.setPeerIdBytes(new Uint8Array(hexToBytes(this.clientId)));
+        this.p2pt.setIdentifier(`_${this.app}_${this.room}`);
 
-      this.p2pt.setIdentifier(`_${this.app}_${this.room}`);
+        this.p2pt.on('peerconnect', (peer) => {
+          //this.p2pt.send(peer, flatbuilder.asUint8Array());
+          this.onDataChannelOpen(peer.id);
+        });
 
-      this.p2pt.on('peerconnect', (peer) => {
-        //this.p2pt.send(peer, flatbuilder.asUint8Array());
-        this.onDataChannelOpen(peer.id);
-      });
+        this.p2pt.on('peerclose', (peer) => {
+          this.onDataChannelClosed(peer.id);
+        });
 
-      this.p2pt.on('peerclose', (peer) => {
-        this.onDataChannelClosed(peer.id);
-      });
+        this.p2pt.on('msg', (peer, msg) => {
+          this.onReceivedData(msg, peer.id);
+        });
 
-      this.p2pt.on('msg', (peer, msg) => {
-        this.onReceivedData(msg, peer.id);
-      });
+        const onConnectHandler = () => {
+          this.onConnectSuccess(this.clientId);
+          this.p2pt.removeListener("trackerconnect", onConnectHandler);
+          res();
+        };
 
-      const onConnectHandler = () => {
-        this.onConnectSuccess(this.clientId);
-        this.p2pt.removeListener("trackerconnect", onConnectHandler);
-        res();
-      };
+        this.p2pt.addListener("trackerconnect", onConnectHandler);
 
-      this.p2pt.addListener("trackerconnect", onConnectHandler);
-
-      this.p2pt.start();
+        this.p2pt.start();
+      })
     });
   }
 
